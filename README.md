6226e9e16bb025c6af8687f6281c8d8fc3c37b64
cancer-sci <bg.king@hotmail.com>
2024-06-15 13:05:53 +0100

    add setup.bat for symlinking

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 ## Overview
 Codebase for analysis of quantitative measures from FDG-PET/CT and DW-MRI scans.
 
+## Symlink to OneDrive
+Data and output folders are kept in OneDrive. Symlink to access.
---
+++ setup.bat
@@ -0,0 +1,34 @@
+@echo off
+setlocal
+
+:: Get the hostname
+set HOSTNAME=%COMPUTERNAME%
+
+echo Device name is %HOSTNAME%
+
+set SCRIPT_DIR=%~dp0
+cd /d %SCRIPT_DIR%
+
+echo Working directory %cd%
+
+:: Check the hostname and execute commands accordingly
+if "%HOSTNAME%"=="BKPC" (
+    echo Identified Home PC ^(BKPC^)
+    mklink /D "data" "D:\Ben King\Documents\OneDrive - University of Edinburgh\projects\MRes\data\" 
+    mklink /D "outputs" "D:\Ben King\Documents\OneDrive - University of Edinburgh\projects\MRes\outputs\"
+) else if "%HOSTNAME%"=="MVM-IGC-D0060" (
+    echo Identified Work PC ^(MVM-IGC-D0060^)
+    mklink /D "data" "C:\Users\s1754085\OneDrive - University of Edinburgh\projects\MRes\data\" 
+    mklink /D "outputs" "C:\Users\s1754085\OneDrive - University of Edinburgh\projects\MRes\outputs\"
+) else if "%HOSTNAME%"=="BKLT" (
+    echo Identified Laptop ^(BKLT^)
+    mklink /D "data" "C:\Users\bgkin\OneDrive - University of Edinburgh\projects\MRes\data\" 
+    mklink /D "outputs" "C:\Users\bgkin\OneDrive - University of Edinburgh\projects\MRes\outputs\"
+) else (
+    echo Device name not matched
+    pause
+)
+
+timeout /t 10
+
+endlocal
