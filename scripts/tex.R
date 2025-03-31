3ab0937ba08ae2032f9328a3e0140380554b63d2
Ben King <75484578+bk1n@users.noreply.github.com>
2025-03-31 13:34:43 +0100

    Full implementation of new diagnostic performance methods
    Implemented new LOO-CV, BS CI, beta testing methods

--- scripts/tex.R
+++ scripts/tex.R
@@ -17,6 +17,46 @@ tex <- list(
     "fec_suvmax_star" = TeX("$\\lbrack^{18}F\\rbrack$FEC PET/CT $SUV_{max}$ STAR")
 )
 
+ylabs <- c(
+    "FDG_SUV_PT" = tex$fdg_suvmax_pt,
+    "FEC_SUV_PT" = tex$fec_suvmax_pt,
+    "ADC_PT" = tex$mri_adc_pt,
+    "FDG_SUV" = tex$fdg_suvmax,
+    "FDG_SA" = "FDG SA (mm)",
+    "FDG_LA" = "FDG LA (mm)",
+    "FDG_NTR" = tex$fdg_suvmax_ntr,
+    "FDG_STAR" = tex$fdg_suvmax_star,
+    "FDG_SNSA" = "FDG SNSA",
+    "FEC_SUV" = tex$fec_suvmax,
+    "FEC_SA" = "FEC SA (mm)",
+    "FEC_LA" = "FEC LA (mm)",
+    "FEC_NTR" = tex$fec_suvmax_ntr,
+    "FEC_STAR" = tex$fec_suvmax_star,
+    "FEC_SNSA" = "FEC SNSA",
+    "ADC" = tex$mri_adc,
+    "ADC_NTR" = tex$mri_adc_ntr
+)
+
+ylabs_norm <- c(
+    "FDG_SUV_PT" = "FDG SUVmax",
+    "FEC_SUV_PT" = "FEC SUVmax",
+    "ADC_PT" = "ADCmean",
+    "FDG_SUV" = "FDG SUVmax",
+    "FDG_SA" = "FDG SA (mm)",
+    "FDG_LA" = "FDG LA (mm)",
+    "FDG_NTR" = "FDG SUVmax NTR",
+    "FDG_STAR" = "FDG STAR",
+    "FDG_SNSA" = "FDG SNSA",
+    "FEC_SUV" = "FEC SUVmax",
+    "FEC_SA" = "FEC SA (mm)",
+    "FEC_LA" = "FEC LA (mm)",
+    "FEC_NTR" = "FEC SUVmax NTR",
+    "FEC_STAR" = "FEC STAR",
+    "FEC_SNSA" = "FEC SNSA",
+    "ADC" = "ADCmean",
+    "ADC_NTR" = "ADCmean NTR"
+)
+
 # tex <- list(
 #     "fdg" = TeX("$\\lbrack^{18}F\\rbrack$FDG PET/CT"),
 #     "fec" = TeX("$\\lbrack^{18}F\\rbrack$FEC PET/CT"),
